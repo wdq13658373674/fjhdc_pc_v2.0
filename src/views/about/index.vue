48db150dc78cd9588fc667bc9578b9d8256de186
wdq <1215971936@qq.com>
2018-06-08 09:21:13 +0800

    dc

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -24,14 +24,11 @@
       <div class="item item4">
         <div class="content">
           <h1>
-            <img src="@/assets/images/about/title1.png" alt="">
+            <img src="@/assets/images/about/title2.png" alt="">
           </h1>
           <div class="con">
             <p>
-              重庆市信力达科技集团有限公司（简称“信力达集团”），信力达集团是一家房地产转型升级的多元化发展集团公司，从事房地产开发10年有余，公司主要从事房地产投资管理、销售服务、物业服务、装修服务、技术研发等。集团公司旗下目前拥有房地产管理平台、房计划定制购房平台、房计划物业服务平台，房计划定制装修平台，为房地产开发企业提供更精细化管理、精准化营销，提升产品竞争力，缩短开发周期，降低企业风险。
-            </p>
-            <p>
-              公司秉持“以客户需求为导向”的经营理念和“用心构筑美好生活”的服务理念，通过技术研发为客户打造一个定制家、智能家、智慧家，为客户提供更优质的服务，让客户买的不止不是房子，还有服务和生活。
+              信力达集团专注房地产投资管理，销售服务、物业服务、装修服务等，为开发企业提供全方位房地产开发服务，开发企业仅需土地的取得及项目前期报建工作。信力达集团通过旗下的房地产管理平台、房计划定制购房平台、房计划物业管理平台，房计划装修平台为开发企业提供精细化管理、精准化营销、提升产品竞争力，缩短开发周期，降低企业风险。信力达集团本作公平、公正、公开的合作理念，实现双方合作共赢的局面。
             </p>
           </div>
         </div>
@@ -42,11 +39,11 @@
         <div class="content clearfix">
           <div class="half-con pull-right">
             <h1>
-              <img src="@/assets/images/about/title1.png" alt="">
+              <img src="@/assets/images/about/tit1.png" alt="">
             </h1>
             <div class="con">
               <p>
-                重庆市信力达科技集团有元化发展集团公司，从事房地产开发10年有余，公司主要从事房地产投资管理、销售服务、物业服务、装修服务、技术研发等。集团公司旗下目前拥有房地产管理平台、房计划定制购房平台、房计划物业服务平台，房计划定制装修平台，为房地产开发企业提供更精细化管理、精准化营销，提升产品竞争力，缩短开发周期，降低企业风险。
+                定制销售服务是信力达集团通过自主研发的“房计划”定制购房平台对潜在客户的需求进行大数据分析，帮助开发企业进行产品的市场定位及定价，提 升产品竞争力，降低开发企业产品设计的滞销风险，缩短销售周期，降低时 间成本和财务成本。房计划定制购房平台以客户需求为导向，借鉴工业 4.0 模式为业主提供可定制化的购房服务 , 让用户充分参与定制自己的家，真正实现我的空间我做主，实现个性化定制。
               </p>
             </div>
           </div>
@@ -57,11 +54,11 @@
         <div class="content clearfix">
           <div class="half-con pull-left">
             <h1>
-              <img src="@/assets/images/about/title1.png" alt="">
+              <img src="@/assets/images/about/tit2.png" alt="">
             </h1>
             <div class="con">
               <p>
-                重庆市信力达科技集团有元化发展集团公司，从事房地产开发10年有余，公司主要从事房地产投资管理、销售服务、物业服务、装修服务、技术研发等。集团公司旗下目前拥有房地产管理平台、房计划定制购房平台、房计划物业服务平台，房计划定制装修平台，为房地产开发企业提供更精细化管理、精准化营销，提升产品竞争力，缩短开发周期，降低企业风险。
+                定制物业服务是信力达集团旗下的房计划物业推出的“金牌管家”定制服务，参照英国标准协会(BSI)服务标准，为业主定制客户服务、家政服务、生活服务、案场服务、特约维修服务等酒店式管家服务。无论是小到更换灯泡，大到房屋装修的问题，业主只要一通电话，专属“管家”都会及时地为业主提供便捷的服务。房计划物业坚持在物业管理的标准化、专业化、精细化道路上砥砺前行。颠覆传统的精细化服务，40 项服务流程，200 条精细化服务标准，4 重检查机制，为业主打造环境优美、秩序井然、安全无忧、邻里和睦的幸福生活社区。
               </p>
             </div>
           </div>
@@ -72,11 +69,11 @@
         <div class="content clearfix">
           <div class="half-con pull-right">
             <h1>
-              <img src="@/assets/images/about/title1.png" alt="">
+              <img src="@/assets/images/about/tit3.png" alt="">
             </h1>
             <div class="con">
               <p>
-                重庆市信力达科技集团有元化发展集团公司，从事房地产开发10年有余，公司主要从事房地产投资管理、销售服务、物业服务、装修服务、技术研发等。集团公司旗下目前拥有房地产管理平台、房计划定制购房平台、房计划物业服务平台，房计划定制装修平台，为房地产开发企业提供更精细化管理、精准化营销，提升产品竞争力，缩短开发周期，降低企业风险。
+                定制生活服务是信力达集团自主研发的“团转到家”智慧生活服务平台，以智能化社区服务为开发方向，与物业硬件相结合，打造智能化社区APP。房计划业主可以在“团转到家”平台上缴纳物业费、水电气费、停车费，享受一键开门、一键洗车、一键锁车等便捷智能化生活服务。团转商城甄选优质水果、生鲜、家用百货，实时配送到家，让业主享受高品质智慧生活服务。
               </p>
             </div>
           </div>
@@ -87,31 +84,184 @@
         <div class="content clearfix">
           <div class="half-con pull-left">
             <h1>
-              <img src="@/assets/images/about/title1.png" alt="">
+              <img src="@/assets/images/about/tit4.png" alt="">
             </h1>
             <div class="con">
               <p>
-                重庆市信力达科技集团有元化发展集团公司，从事房地产开发10年有余，公司主要从事房地产投资管理、销售服务、物业服务、装修服务、技术研发等。集团公司旗下目前拥有房地产管理平台、房计划定制购房平台、房计划物业服务平台，房计划定制装修平台，为房地产开发企业提供更精细化管理、精准化营销，提升产品竞争力，缩短开发周期，降低企业风险。
+                定制装修服务是信力达集团旗下的房计划物业推出的房屋定制装修服务，基于互联网装修 O2O 模式，线上为业主提供不同风格装修设计方案，线下资深设计团队全程参与设计服务，以业主的视角提供一站式全屋装修定制，实现个性化定制。房计划物业郑重承诺:“三年保修，终身维保”，让业主省钱、省时、省心。
               </p>
             </div>
           </div>
         </div>
       </div>
-      <div class="item item9"></div>
+      <div class="item item91"></div>
 
       <!--成功案例-->
       <!--云鼎府邸-->
       <div class="item item10">
-
+        <div class="content">
+          <div class="project-con pull-left">
+            <h1>云鼎府邸</h1>
+            <ul class="pro-list">
+              <li>
+                <span class="span1">项目地址</span>
+                <span class="span2">四川省邻水县西部新城</span>
+              </li>
+              <li>
+                <span class="span1">物业类型</span>
+                <span class="span2">洋房</span>
+              </li>
+              <li>
+                <span class="span1">建筑面积</span>
+                <span class="span2">约65000m<sup>2</sup></span>
+              </li>
+              <li>
+                <span class="span1">房源套数</span>
+                <span class="span2">约420套</span>
+              </li>
+              <li>
+                <span class="span1">开发商</span>
+                <span class="span2">邻水县方居房地产开发有限公司</span>
+              </li>
+            </ul>
+            <div class="data">
+              <img src="@/assets/images/about/data1.png" alt="">
+            </div>
+          </div>
+        </div>
       </div>
       <!--御临天下-->
-      <div class="item item11"></div>
+      <div class="item item11">
+        <div class="content">
+          <div class="project-con pull-right">
+            <h1>御临天下</h1>
+            <ul class="pro-list">
+              <li>
+                <span class="span1">项目地址</span>
+                <span class="span2">四川省邻水县西部新城</span>
+              </li>
+              <li>
+                <span class="span1">物业类型</span>
+                <span class="span2">高层</span>
+              </li>
+              <li>
+                <span class="span1">建筑面积</span>
+                <span class="span2">约100000m<sup>2</sup></span>
+              </li>
+              <li>
+                <span class="span1">房源套数</span>
+                <span class="span2">约750套</span>
+              </li>
+              <li>
+                <span class="span1">开发商</span>
+                <span class="span2">邻水县荣恒房地产开发有限公司</span>
+              </li>
+            </ul>
+            <div class="data">
+              <img src="@/assets/images/about/data2.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
       <!--西城御府-->
-      <div class="item item12"></div>
+      <div class="item item12">
+        <div class="content">
+          <div class="project-con pull-left">
+            <h1>西城御府</h1>
+            <ul class="pro-list">
+              <li>
+                <span class="span1">项目地址</span>
+                <span class="span2">四川省邻水县西部新城邻水二中后门</span>
+              </li>
+              <li>
+                <span class="span1">物业类型</span>
+                <span class="span2">高层、洋房</span>
+              </li>
+              <li>
+                <span class="span1">建筑面积</span>
+                <span class="span2">约260000m<sup>2</sup></span>
+              </li>
+              <li>
+                <span class="span1">房源套数</span>
+                <span class="span2">约1700套</span>
+              </li>
+              <li>
+                <span class="span1">开发商</span>
+                <span class="span2">邻水县亿德顺置业有限公司</span>
+              </li>
+            </ul>
+            <div class="data">
+              <img src="@/assets/images/about/data3.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
       <!--东樾府-->
-      <div class="item item13"></div>
+      <div class="item item13">
+        <div class="content">
+          <div class="project-con pull-right">
+            <h1>东樾府</h1>
+            <ul class="pro-list">
+              <li>
+                <span class="span1">项目地址</span>
+                <span class="span2">武胜东部新区东方街与仁和六街交叉处</span>
+              </li>
+              <li>
+                <span class="span1">物业类型</span>
+                <span class="span2">高层、洋房</span>
+              </li>
+              <li>
+                <span class="span1">建筑面积</span>
+                <span class="span2">约163000m<sup>2</sup></span>
+              </li>
+              <li>
+                <span class="span1">房源套数</span>
+                <span class="span2">约1130套</span>
+              </li>
+              <li>
+                <span class="span1">开发商</span>
+                <span class="span2">武胜恒阳房地产开发有限公司</span>
+              </li>
+            </ul>
+            <div class="data">
+              <img src="@/assets/images/about/data4.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
       <!--诚远御府-->
-      <div class="item item14"></div>
+      <div class="item item14">
+        <div class="content">
+          <div class="project-con pull-left">
+            <h1>云鼎府邸</h1>
+            <ul class="pro-list">
+              <li>
+                <span class="span1">项目地址</span>
+                <span class="span2">贵州惠水县人民北路（原水利局）</span>
+              </li>
+              <li>
+                <span class="span1">物业类型</span>
+                <span class="span2">高层、洋房</span>
+              </li>
+              <li>
+                <span class="span1">建筑面积</span>
+                <span class="span2">约131475m<sup>2</sup></span>
+              </li>
+              <li>
+                <span class="span1">房源套数</span>
+                <span class="span2">约836套</span>
+              </li>
+              <li>
+                <span class="span1">开发商</span>
+                <span class="span2">惠水城投集团城远置业开发有限责任公司</span>
+              </li>
+            </ul>
+            <div class="data">
+              <img src="@/assets/images/about/data5.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
 
       <!--战略合作伙伴-->
       <div class="item item15"></div>
